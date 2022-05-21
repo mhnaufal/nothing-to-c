6015a8908a741d55d6dfc24429425296c9adf5b4
mhnaufal <aal.pirates16@gmail.com>
2022-05-21 17:30:21 +0700

    main

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define SCORE 10
+
+int main(int argc, char *argv[])
+{
+    int score = 23;
+
+    if (SCORE >= 6)
+    {
+        printf("Comes from WSL Ubuntu \n");
+    }
+
+    return 0;
+}
