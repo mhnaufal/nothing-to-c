d9387c2302cf6e7c6e2ebeb4b9c3e498efd6fa92
mhnaufal <aal.pirates16@gmail.com>
2022-05-27 09:55:07 +0700

    keys all

--- not-redis/command.c
+++ not-redis/command.c
@@ -4,89 +4,7 @@
 #include <string.h>
 #include <errno.h>
 
-/* DATAS */
-enum database_commands
-{
-    GET,
-    SET,
-    KEYS
-};
-
-/**
- * 'static' here means that this const
- * only available within this unit/file
- */
-static const char *const commands[] = {
-    [GET] = "get",
-    [SET] = "set",
-    [KEYS] = "keys",
-};
-
-/* METHODS */
-void show_instruction()
-{
-    printf(
-        "db [command] [params]\n\n"
-        "📌 get    : take a key as the parameter and return the value if the given key is exists\n"
-        "📌 set    : take a key and a value as the paramters and save it to the file db\n"
-        "📌 keys   : take a pattern of key and return the value base on the pattern\n");
-}
-
-FILE *open_database_file(char *file_path, char *mode)
-{
-    FILE *opened_file = fopen(file_path, mode);
-
-    if (opened_file == NULL)
-    {
-        fprintf(stderr, "ERROR: could not open file \"%s\": %m\n", file_path);
-        exit(1);
-    }
-
-    return opened_file;
-}
-
-void show_get(char *key)
-{
-    if (!key)
-    {
-        fprintf(stderr, "ERROR: no key given\n");
-        exit(1);
-    }
-
-    FILE *db_file = open_database_file("db.txt", "r");
-
-    char datas[255];
-
-    // make it read per string, instead of per newline
-    while (fgets(datas, sizeof(datas), db_file) != NULL)
-    {
-        printf("[%s] %s", key, datas);
-        if (!strcmp(key, datas))
-        {
-            printf("[%s] %s", key, datas);
-        }
-    }
-
-    puts("");
-    fclose(db_file);
-}
-
-void show_keys(char *pattern)
-{
-    FILE *db_file = open_database_file("db.txt", "r");
-
-    // compare the pattern
-
-    char datas[255];
-
-    while (fgets(datas, sizeof(datas), db_file) != NULL)
-    {
-        fputs(datas, stdout);
-    }
-
-    puts("");
-    fclose(db_file);
-}
+#include "command.h"
 
 /* MAIN */
 int main(int argc, char *argv[])
@@ -109,15 +27,95 @@ int main(int argc, char *argv[])
     }
     else if (!strcmp(command, commands[KEYS]))
     {
-        char *pattern = "*";
+        char *pattern;
+
+        // To prevent from giving a NULL argument into show_keys() function
+        if (key_param == NULL)
+        {
+            pattern = "";
+        }
+        else
+        {
+            pattern = key_param;
+        }
+
         show_keys(pattern);
     }
     else
     {
-        printf("[❌] Can't continue the process. Undefined command \"%s\"\n", command);
+        printf("[❌] Can't continue the process. Undefined command '%s'\n", command);
     }
 
     puts("-OK-\n");
 
     return 0;
+}
+
+void show_instruction()
+{
+    printf(
+        "db [command] [params]\n\n"
+        "📌 get    : take a key as the parameter and return the value if the given key is exists\n"
+        "📌 set    : take a key and a value as the paramters and save it to the file db\n"
+        "📌 keys   : take a pattern of key and return the value base on the pattern\n");
+}
+
+FILE *open_database_file(char *file_path, char *mode)
+{
+    FILE *opened_file = fopen(file_path, mode);
+
+    if (opened_file == NULL)
+    {
+        fprintf(stderr, "[❌] Can't not open file '%s': %m\n", file_path);
+        exit(1);
+    }
+
+    return opened_file;
+}
+
+void show_get(char *key)
+{
+    if (!key)
+    {
+        fprintf(stderr, "[❌] Can't process 'get' command. No key provided\n");
+        exit(1);
+    }
+
+    FILE *db_file = open_database_file("db.txt", "r");
+
+    char datas[BUFF_SIZE];
+
+    while (fgets(datas, sizeof(datas), db_file) != NULL)
+    {
+        printf("[%s] %s", key, datas);
+        if (!strcmp(key, datas))
+        {
+            printf("[%s] %s", key, datas);
+        }
+    }
+
+    puts("");
+    fclose(db_file);
+}
+
+void show_keys(char *pattern)
+{
+    FILE *db_file = open_database_file("db.txt", "r");
+
+    char datas[BUFF_SIZE];
+
+    if (!strcmp(pattern, "all") || pattern == "")
+    {
+        while (fgets(datas, sizeof(datas), db_file) != NULL)
+        {
+            fputs(datas, stdout);
+        }
+    }
+    else
+    {
+        printf("[❌] Can't process 'keys' command. Unknown options '%s'", pattern);
+    }
+
+    puts("");
+    fclose(db_file);
 }
---
+++ not-redis/command.h
@@ -0,0 +1,30 @@
+#ifndef COMMAND_H
+#define COMMAND_H
+
+#define BUFF_SIZE 1024
+
+/* DATAS */
+enum database_commands
+{
+    GET,
+    SET,
+    KEYS
+};
+
+/**
+ * 'static' here means that this const
+ * only available within this unit/file
+ */
+static const char *const commands[] = {
+    [GET] = "get",
+    [SET] = "set",
+    [KEYS] = "keys",
+};
+
+/* METHODS */
+void show_instruction();
+void show_get(char *key);
+void show_keys(char *pattern);
+FILE *open_database_file(char *file_path, char *mode);
+
+#endif
