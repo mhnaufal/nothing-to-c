abffb20f6a32d2028d900454bd89c7e165c3985c
mhnaufal <aal.pirates16@gmail.com>
2022-06-01 19:48:43 +0700

    init compiler

---
+++ compiler/parser.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdbool.h>
+
+#include "parser.h"
+
+int main(int argc, char *argv[])
+{
+    printf("%d\n", token_eof);
+    return 0;
+}
---
+++ compiler/parser.h
@@ -0,0 +1,9 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+enum Token
+{
+    token_eof = -1
+};
+
+#endif
