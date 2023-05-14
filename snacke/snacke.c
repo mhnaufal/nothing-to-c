9403fe81ebaa885e822fc859b1df8e1571f36792
mhnaufal <aal.pirates16@gmail.com>
2023-05-14 21:38:34 +0700

    snake movement and display board

--- snacke/snacke.c
+++ snacke/snacke.c
@@ -17,5 +17,59 @@ int main(int argc, char *argv[]) {
     return EXIT_SUCCESS;
   }
 
+  do {
+    system("cls");
+
+    init_board();
+    display_board();
+
+    movement();
+
+  } while(score < 5);
+
   return EXIT_SUCCESS;
+}
+
+void init_board()
+{
+  for (int i = 0; i < HEIGHT; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      BOARD[i][j] = ' ';
+    }
+  }
+  BOARD[PLAYER_X][PLAYER_Y] = HEAD;
+}
+
+void display_board()
+{
+  for (int i = 0; i < HEIGHT; i++) {
+    for (int j = 0; j < WIDTH; j++) {
+      printf("%c", BOARD[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void movement()
+{
+  move = getch();
+
+  switch (move) {
+    case 'w':
+      PLAYER_X = PLAYER_X - 1;
+      BOARD[PLAYER_X][PLAYER_Y] = HEAD;
+      break;
+    case 's':
+      PLAYER_X = PLAYER_X + 1;
+      BOARD[PLAYER_X][PLAYER_Y] = HEAD;
+      break;
+    case 'a':
+      PLAYER_Y = PLAYER_Y - 1;
+      BOARD[PLAYER_X][PLAYER_Y] = HEAD;
+      break;
+    case 'd':
+      PLAYER_Y = PLAYER_Y + 1;
+      BOARD[PLAYER_X][PLAYER_Y] = HEAD;
+      break;
+  }
 }
--- snacke/snacke.h
+++ snacke/snacke.h
@@ -5,3 +5,24 @@
 #include <string.h>
 #include <time.h>
 #include <stdbool.h>
+#include <conio.h>
+#include <unistd.h>
+
+int const HEIGHT = 10;
+int const WIDTH = 20;
+static char BOARD[HEIGHT][WIDTH];
+int PLAYER_X = (HEIGHT / 2) - 1;
+int PLAYER_Y = (WIDTH / 2) - 1;
+
+char const HEAD = 'X';
+char const UPWARD = '^';
+char const DOWNWARD = 'v';
+char const RIGHT = '>';
+char const LEFT = '<';
+
+int score = 0;
+char move;
+
+void init_board();
+void display_board();
+void movement();
