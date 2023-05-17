fedba948a6673711663969612ccd8ba4a3c27ce0
mhnaufal <aal.pirates16@gmail.com>
2023-05-18 06:44:03 +0700

    update surface and rectangle

---
+++ sdl2-game/platformer/README.md
@@ -0,0 +1,28 @@
+# Platformer 🙀
+
+_soon_
+
+## Installation
+
+_soon_
+
+---
+
+## How To Play
+
+_soon_
+
+---
+
+## Future Works
+
+_soon_
+
+---
+
+
+## References
+
+1. https://lazyfoo.net/tutorials/SDL/01_hello_SDL/index2.php
+2. https://wiki.libsdl.org/SDL2/
+3. https://www.parallelrealities.co.uk/tutorials/ppp/ppp1.php
---
+++ sdl2-game/platformer/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+#define WINDOW_WIDTH 640 * 1.5
+#define WINDOW_HEIGHT 480 * 1.5
+
+int main(int argc, char *argv[])
+{
+    // Initialize window
+    SDL_Window *window = NULL;
+
+    // Initialize surface
+    SDL_Surface *surface = NULL;
+
+    // Initialize SDL2
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) != 0)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to start SDL2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "SDL2 is ready!\n");
+
+    // Create window
+    window = SDL_CreateWindow("Platformer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == NULL)
+    {
+        SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Failed to load the window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // Put on the surface
+    surface = SDL_GetWindowSurface(window);
+
+    // Fill the surface with white
+    SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0x00, 0xFF, 0xFF));
+
+    SDL_Rect player;
+    player.h = 100;
+    player.w = 100;
+    player.x = WINDOW_WIDTH / 2 - player.w / 2;
+    player.y = WINDOW_HEIGHT / 2 - player.h / 2;
+
+    SDL_FillRect(surface, &player, SDL_MapRGB(surface->format, 252, 7, 23));
+
+    // Update the surface
+    SDL_UpdateWindowSurface(window);
+
+    // Make window stay still
+    // SDL_Event e;
+    // bool quit = false;
+    // while (quit == false)
+    // {
+    //     while (SDL_PollEvent(&e))
+    //     {
+    //         if (e.type == SDL_Quit)
+    //             quit = true;
+    //     }
+    // }
+    SDL_Delay(3000);
+    // sleep(1);
+
+    // Clean up everything
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
