e44cf065a3b85b65f20711d8c87f20bf187d9491
mhnaufal <aal.pirates16@gmail.com>
2023-05-26 10:12:51 +0700

    tilemap

--- sdl2-game/platformer/include/headers/main.h
+++ sdl2-game/platformer/include/headers/main.h
@@ -1,13 +1,14 @@
 #pragma once
 
+#include <map.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
 
-const int WINDOW_WIDTH = 640 * 1.5;
-const int WINDOW_HEIGHT = 480 * 1.5;
+const int WINDOW_WIDTH = 640 ;
+const int WINDOW_HEIGHT = 480;
 const int PIXEL_WIDTH = 32 * 2;
 const int PIXEL_HEIGHT = 32 * 2;
 const int FPS = 60;
@@ -37,12 +38,25 @@ typedef struct
     SDL_Texture *texture;
 } Entity;
 
+typedef struct
+{
+    int tile[TILE_HEIGHT][TILE_WIDTH];
+    SDL_Texture *grass;
+    SDL_Texture *rock;
+    SDL_Texture *water;
+} Map;
+
 bool initSDL();
 bool initWindow(SDLGame *p_sdl_game, const char *p_title);
 void cleanUpSDL(SDLGame *p_sdl_game);
 
 SDL_Texture *loadTexture(SDLGame *p_sdl_game, const char *p_path);
+void clearRenderer(SDLGame *p_sdl_game);
+void renderTexture(SDLGame *p_sdl_game);
 
 Entity initEntity(SDL_Rect p_rect, SDL_Texture *p_texture);
 void drawEntity(SDLGame *p_sdl_game, Entity *p_entity);
 void updateEntity(Entity *p_entity, SDL_Rect p_rect);
+
+Map loadMap(SDLGame *p_sdl_game);
+void drawMap(Map *p_map, SDLGame *p_sdl_game);
--- sdl2-game/platformer/main.c
+++ sdl2-game/platformer/main.c
@@ -15,6 +15,8 @@ int main(int argc, char *argv[])
     SDL_Texture *texture1 = loadTexture(&sdl_game, "./res/gfx/cat1.png");
     Entity cat1 = initEntity(entity_size, texture1);
 
+    Map map1 = loadMap(&sdl_game);
+
     SDL_Event event;
 
     while (!game_state.Exit)
@@ -29,9 +31,13 @@ int main(int argc, char *argv[])
             }
         }
 
+        clearRenderer(&sdl_game);
+
+        drawMap(&map1, &sdl_game);
+        drawEntity(&sdl_game, &cat1);
         updateEntity(&cat1, new_size);
 
-        drawEntity(&sdl_game, &cat1);
+        renderTexture(&sdl_game);
 
         FRAME_TIME = SDL_GetTicks() - FRAME_START;
 
@@ -104,6 +110,16 @@ SDL_Texture *loadTexture(SDLGame *p_sdl_game, const char *p_path)
     return texture;
 }
 
+void clearRenderer(SDLGame *p_sdl_game)
+{
+    SDL_RenderClear(p_sdl_game);
+}
+
+void renderTexture(SDLGame *p_sdl_game)
+{
+    SDL_RenderPresent(p_sdl_game->renderer);
+}
+
 Entity initEntity(SDL_Rect p_rect, SDL_Texture *p_texture)
 {
     Entity ent = {&p_rect, p_texture};
@@ -113,13 +129,10 @@ Entity initEntity(SDL_Rect p_rect, SDL_Texture *p_texture)
 
 void drawEntity(SDLGame *p_sdl_game, Entity *p_entity)
 {
-    SDL_RenderClear(p_sdl_game->renderer);
-
     SDL_Rect src = {0, 0, p_entity->frame->w, p_entity->frame->h};
     SDL_Rect dst = {p_entity->frame->x, p_entity->frame->y, p_entity->frame->w, p_entity->frame->h};
 
     SDL_RenderCopy(p_sdl_game->renderer, p_entity->texture, &src, &dst);
-    SDL_RenderPresent(p_sdl_game->renderer);
 }
 
 void updateEntity(Entity *p_entity, SDL_Rect p_rect)
@@ -128,4 +141,47 @@ void updateEntity(Entity *p_entity, SDL_Rect p_rect)
     p_entity->frame->h += p_rect.h;
     p_entity->frame->x += p_rect.x;
     p_entity->frame->y += p_rect.y;
+}
+
+Map loadMap(SDLGame *p_sdl_game)
+{
+    SDL_Rect src = {0, 0, 32, 32};
+    SDL_Rect dst = {32, 32, 32, 32};
+
+    Map map;
+    map.grass = loadTexture(p_sdl_game, "./res/gfx/dirt.png");
+    map.rock = loadTexture(p_sdl_game, "./res/gfx/rock.png");
+    map.water = loadTexture(p_sdl_game, "./res/gfx/water.png");
+
+    return map;
+}
+
+void drawMap(Map *p_map, SDLGame *p_sdl_game)
+{
+    int current_tile = 0;
+    SDL_Rect src = {0, 0, 32, 32};
+    SDL_Rect dst = {0, 0, 32, 32};
+
+    for (int i = 0; i < TILE_HEIGHT; i++)
+    {
+        for (int j = 0; j < TILE_WIDTH; j++)
+        {
+            p_map->tile[i][j] = level1[i][j];
+            dst.x = j * 32;
+            dst.y = i * 32;
+
+            switch (p_map->tile[i][j])
+            {
+            case 0:
+                SDL_RenderCopy(p_sdl_game->renderer, p_map->grass, &src, &dst);
+                break;
+            case 1:
+                SDL_RenderCopy(p_sdl_game->renderer, p_map->rock, &src, &dst);
+                break;
+            case 2:
+                SDL_RenderCopy(p_sdl_game->renderer, p_map->water, &src, &dst);
+                break;
+            }
+        }
+    }
 }
