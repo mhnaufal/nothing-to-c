9f4a0faf98f3bc022c2ea5c1ab013e7d16d003a8
mhnaufal <aal.pirates16@gmail.com>
2023-05-30 06:01:22 +0700

    assembly hello world

--- assembly/README.md
+++ assembly/README.md
@@ -8,7 +8,14 @@ gcc intro.o
 ./a.exe
 ```
 
+```bash
+nasm -f win64 -o build/intro.obj intro.asm && gcc -o build/intro build/intro.obj && ./build/intro.exe
+```
+
 ## References
 
 - [Syscall Table](https://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/)
 - https://cs.lmu.edu/~ray/notes/nasmtutorial/
+- https://sonictk.github.io/asm_tutorial/
+- https://www.ic.unicamp.br/~pannain/mc404/aulas/pdfs/Art%20Of%20Intel%20x86%20Assembly.pdf
+- https://www.nasm.us/xdoc/2.16.01/nasmdoc.pdf
--- assembly/intro.asm
+++ assembly/intro.asm
@@ -1,11 +1,23 @@
-global  main
-        extern  puts
-        section .text
-main:
-        sub     rsp, 28h                        ; Reserve the shadow space
-        mov     rcx, message                    ; First argument is address of message
-        call    puts                            ; puts(message)
-        add     rsp, 28h                        ; Remove shadow space
-        ret
-message:
-        db      'Hello', 0                      ; C strings need a zero byte at the end
+bits 64 ; tell nasm to run on 64-bit hardware
+default rel ; tell nasm to compile the program with rip-relative addresing
+
+; data segment
+segment .data
+        msg db "Hello, Assembly!", 0xd, 0xa, 0 ; define a variable called "msg", of type "define byte" or "byte" with the value of "Hello, Assembly!" followed by 'CR' and 'LF' as hexa and a null terminator
+
+; text segment
+segment .text
+global main ; export symbol
+extern ExitProcess ; import symbol
+extern printf ; import symbol
+
+main: ; program entry poin
+        push rbp ; put the base pointer (rbp) to the bottom of the stack
+        mov rbp, rsp ; move the stack pointer (rsp) to the current base pointer (rbp)
+        sub rsp, 32 ; subtract rsp - 32 to give some shadow space for Microsoft x64. Help us to debug in an easier way
+
+        lea rcx, [msg] ; move the pointer of [msg] to rcx (the function parameter stack)
+        call printf ; print the first argument in the function parameter stack
+
+        xor rax, rax ; zero-ing it out the rax register
+        call ExitProcess
