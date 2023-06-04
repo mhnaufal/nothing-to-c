4eae3c63746342015c0fb6c8c819525bf4a61d37
mhnaufal <aal.pirates16@gmail.com>
2023-06-04 17:13:59 +0700

    player rectangle

--- raylib-game/brawlhahaha/src/main.c
+++ raylib-game/brawlhahaha/src/main.c
@@ -11,6 +11,10 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
     Texture2D tile = LoadTexture("../assets/img/tile/dungeon.png");
     Font font = LoadFont("../assets/font/opensans.ttf");
 
+    Vector2 player_position = {(float)100, (float)100};
+    float player_velocity = 10;
+    float gravity = 0.3;
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -25,6 +29,17 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
             DrawTextureRec(tile, (Rectangle){0, 0, 64, 64}, (Vector2){i, SCREEN_HEIGHT - 140}, WHITE);
             DrawTextureRec(tile, (Rectangle){0, 0, 64, 64}, (Vector2){i, SCREEN_HEIGHT - 100}, WHITE);
             DrawTextureRec(tile, (Rectangle){0, 0, 64, 64}, (Vector2){i, SCREEN_HEIGHT - 60}, WHITE);
+        }
+
+        DrawRectangleV(player_position, (Vector2){100, 180}, RED);
+        player_position.y += player_velocity;
+        if (player_position.y + 180 + player_velocity >= SCREEN_HEIGHT - 125)
+        {
+            player_velocity = 0;
+        }
+        else
+        {
+            player_velocity += gravity;
         }
 
         DrawText("Brawlhahaha", SCREEN_WIDTH / 2 - 100, 10, 40, BLACK);
