532cacb210c1bf9acee6c736d0601e3458dd4fa6
mhnaufal <aal.pirates16@gmail.com>
2023-06-08 20:42:33 +0700

    health system

--- raylib-game/brawlhahaha/src/main.c
+++ raylib-game/brawlhahaha/src/main.c
@@ -7,11 +7,13 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Brawlhahaha");
     InitAudioDevice();
 
+    /* TODO: refactor texture laoding */
     Texture2D background = LoadTexture("../assets/img/background/moon.png");
     Texture2D tile = LoadTexture("../assets/img/tile/dungeon.png");
     Music music_bg = LoadMusicStream("../assets/audio/war.mp3");
     Sound sound_start = LoadSound("../assets/audio/ready-fight.mp3");
 
+    /* TODO: refactor entity initialization */
     EntityMap em1;
     EntityMap em2;
 
@@ -40,11 +42,28 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
         emg1->entities[1] = em2;
     */
 
+    /* TODO: refactor health system */
+    Rectangle base_health1 = {0, 0, SCREEN_WIDTH / 2, 50};
+    float rem1 = 0;
+    Rectangle health1 = {rem1 * 100, 0, SCREEN_WIDTH / 2 - 0, 50};
+
+    Rectangle base_health2 = {SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, 50};
+    float rem2 = 0;
+    Rectangle health2 = {SCREEN_WIDTH / 2, 0, SCREEN_WIDTH / 2, 50};
+
     SetTargetFPS(60);
 
     PlaySound(sound_start);
     while (!WindowShouldClose())
     {
+        /* TODO: health reducing system */
+        rem1++;
+        health1.x = rem1;
+        health1.width = SCREEN_WIDTH / 2 - rem1;
+
+        rem2++;
+        health2.width -= rem2 / 100;
+
         /* Audio */
         SetMusicVolume(music_bg, 0.2);
         UpdateMusicStream(music_bg);
@@ -79,7 +98,11 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
             DrawRectangleV((Vector2){e2.m_transform->position.x - e2.m_size->width + e1.m_size->width, e2.m_transform->position.y}, (Vector2){e2.m_size->width, e2.m_size->height}, GREEN);
         }
 
-        DrawText("Brawlhahaha", SCREEN_WIDTH / 2 - 100, 10, 40, BLACK);
+        DrawRectangleRec(base_health1, YELLOW);
+        DrawRectangleRec(health1, RED);
+        DrawRectangleRec(base_health2, GREEN);
+        DrawRectangleRec(health2, BLUE);
+        DrawText("Brawlhahaha", SCREEN_WIDTH / 2 - 100, 10, 40, WHITE);
         DrawFPS(SCREEN_WIDTH - 100, 10);
         EndDrawing();
     }
